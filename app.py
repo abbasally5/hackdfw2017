ccf3794864aa427bd98edae736db3d962ff55f8e
root <root@ip-172-31-28-202.ec2.internal>
2017-04-22 16:40:02 +0000

    now reads in user input and returns tweets related to that hashtag

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, jsonify, Response
+
+from test import *
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,7 +13,15 @@ def hello():
 @app.route("/hashtag", methods=['POST'])
 def hashtag():
     hashtag_text = request.form.get('hashtag', None)
-    return redirect(url_for('hello')) 
+    api = TwitterClient()
+    tweets_dict = api.get_tweets(query = hashtag_text, count = 2)
+    #print(tweets_dict)
+    #tweets = [json.dumps(tweet) for tweet in tweets_dict]
+    #print json.dumps(tweets_dict)
+    return jsonify(tweets=tweets_dict)
+    #return json.dumps(tweets_dict)
+    #return redirect(url_for('hello')) 
+    #return Response(json.dumps(tweets_dict), mimetype='application/json')
 
 
 if __name__ == "__main__":
